116a1398c60bc035b34cef3079c6da678116078a
David Jones <dgjones@gmail.com>
2016-03-14 00:39:47 -0700

    work on setting up webp

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function(grunt) {
     require('jit-grunt')(grunt);
 
     grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-webp');
  
     grunt.initConfig({
         app: {
@@ -74,6 +75,38 @@ module.exports = function(grunt) {
                     auto : true
                 }
             }
+        },
+
+        webp: {
+          files: {
+            //expand: true,
+            //cwd: 'path/to/source/images',
+            src: 'images/**/*.{jpg,jpeg,png,gif}',
+            dest: ''
+          },
+          options: {
+            binpath: require('webp-bin').path,
+            preset: 'photo',
+            verbose: true,
+            quality: 80,
+            alphaQuality: 80,
+            compressionMethod: 6,
+            segments: 4,
+            psnr: 42,
+            sns: 50,
+            filterStrength: 40,
+            filterSharpness: 3,
+            simpleFilter: true,
+            partitionLimit: 50,
+            analysisPass: 6,
+            multiThreading: true,
+            lowMemory: false,
+            alphaMethod: 0,
+            alphaFilter: 'best',
+            alphaCleanup: true,
+            noAlpha: false,
+            lossless: false
+          }
         },
 
         imagemin: {
